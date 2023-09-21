068c07e89e56d9f980f17942574f2ea1980b0ba3
Smars <Smars@innospot.io>
2023-09-22 00:25:09 +0800

    refactor: value type

--- innospots-base/src/main/java/io/innospots/base/model/field/FieldValueType.java
+++ innospots-base/src/main/java/io/innospots/base/model/field/FieldValueType.java
@@ -22,10 +22,18 @@ import lombok.Getter;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 字段值类型
@@ -40,56 +48,216 @@ public enum FieldValueType {
     /**
      *
      */
-    FIELD_CODE("fld", "refer", "field code identity", String.class, "VARCHAR",null),
-    STRING("string", "string", "character string", String.class, "VARCHAR",null),
-    INTEGER("int", "number", "integer", Integer.class, "INT",new String[]{"^[-]{0,1}([1-9]\\d{1,8})"}),
-    BOOLEAN("boolean", "number", "boolean true or false", Boolean.class, "INT",null),
-    LONG("long", "number", "long", Long.class, "BIGINT",new String[]{"^[-]{0,1}([1-9]\\d{9,19})[lL]{0,1}"}),
-    DATE("date", "string", "yyyy-MM-dd", LocalDate.class, "DATE",new String[]{"^([1-9]\\d{3})[-/.](0[1-9]|1[0-2])[-/.](0[1-9]|[1-2][0-9]|3[0-1])"}),
-    TIME("time", "string", "HH:mm", LocalTime.class, "DATETIME",new String[]{"^(20|21|22|23|[0-1]\\d):([0-5]\\d)(?:\\:([0-5]\\d)){0,1}(?:\\.(\\d{3})){0,1}"}),
-    DATE_TIME("datetime", "string", "yyyy-MM-dd HH:mm:ss", LocalDateTime.class, "DATETIME",new String[]{"^([1-9]\\d{3})[-/.](0[1-9]|1[0-2])[-/.](0[1-9]|[1-2][0-9]|3[0-1])[ |T](20|21|22|23|[0-1]\\d):([0-5]\\d)(?:\\:([0-5]\\d)){0,1}(?:\\.(\\d{3})){0,1}"}),
-    DOUBLE("double", "number", "00.0", Double.class, "DOUBLE",new String[]{"^[-]{0,1}(\\d+)[.]{0,1}(\\d+)[dD]{0,1}"}),
-    CURRENCY("currency", "number", "bigDecimal format", BigDecimal.class, "DECIMAL",new String[]{"^[-]{0,1}([￥$€￡￠])(\\d+)[.]{0,1}(\\d+)"}),
-    TIMESTAMP("timestamp", "number", "millisecond", Long.class, "TIMESTAMP",null),
-    YEAR_MONTH("ym", "number", "yyyyMM", Integer.class, "INT",new String[]{"^([1-9]\\d{3})(0[1-9]|1[0-2])"}),
-    YEAR_MONTH_DATE("ymd", "number", "yyyyMMdd", Integer.class, "INT",new String[]{"^([1-9]\\d{3})(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])"}),
-    MONTH("mm", "number", "MM", Integer.class, "INT",null),
-    MONTH_DATE("md", "number", "MMdd", Integer.class, "INT",null),
-    DAY_OF_MONTH("dm", "number", "dd", Integer.class, "INT",null),
-    MAP("map", "json", "", Map.class, "TEXT",null),
-    LIST("list", "list", "", List.class, "TEXT",null),
-    COLLECTION("seq", "list", "list,set, array", Collection.class, "TEXT",null),
-    NUMBER("number", "number", "int,double float,long", Number.class, "DOUBLE",null),
-    DECIMAL("decimal", "number", "decimal type", BigDecimal.class, "DECIMAL",null),
-    SET("set", "list", "", Set.class, "TEXT",null),
-    OBJECT("object", "json", "", Object.class, "Blob",null),
-    FILE("object", "json", "", File.class, "Blob",null),
+    FIELD_CODE("fld", "refer", "field code identity", String.class, "VARCHAR",
+            null, (p, v) -> v),
 
-    // TODO dataview variable use
-    NUMERIC("numeric", "number", "00.0", Double.class, "DOUBLE",null), // TODO
-    FRAGMENT("string", "string", "character string", String.class, "VARCHAR",null);
+    STRING("string", "string", "character string", String.class, "VARCHAR",
+            null, (p, v) -> v),
+
+    INTEGER("int", "number", "integer", Integer.class, "INT",
+            Pattern.compile("^[-]{0,1}([1-9]\\d{1,8})"), (p, v) -> {
+        if (v instanceof Number) {
+            return ((Number) v).intValue();
+        }
+        return Integer.valueOf(v.toString());
+    }),
+
+    BOOLEAN("boolean", "number", "boolean true or false", Boolean.class, "INT",
+            null, (p, v) -> {
+        if (v == null) {
+            return Boolean.FALSE;
+        }
+        return Boolean.valueOf(v.toString());
+    }),
+
+    LONG("long", "number", "long", Long.class, "BIGINT",
+            Pattern.compile("^[-]{0,1}([1-9]\\d{9,19})[lL]{0,1}"), (p, v) -> {
+        if (v instanceof Number) {
+            return ((Number) v).longValue();
+        }
+        return Long.valueOf(v.toString());
+    }),
+
+    DATE("date", "string", "yyyy-MM-dd", LocalDate.class, "DATE",
+            Pattern.compile("^([1-9]\\d{3})[-/.](0[1-9]|1[0-2])[-/.](0[1-9]|[1-2][0-9]|3[0-1])"), (p, v) -> {
+        Matcher m = p.matcher(v.toString());
+        LocalDate localDate = null;
+        while (m.find()) {
+            int y = Integer.parseInt(m.group(1));
+            int mh = Integer.parseInt(m.group(2));
+            int d = Integer.parseInt(m.group(3));
+            localDate = LocalDate.of(y, mh, d);
+        }
+        return localDate;
+    }),
+
+    TIME("time", "string", "HH:mm", LocalTime.class, "DATETIME",
+            Pattern.compile("^(20|21|22|23|[0-1]\\d):([0-5]\\d)(?:\\:([0-5]\\d)){0,1}(?:\\.(\\d{3})){0,1}"), (p, v) -> {
+        Matcher m = p.matcher(v.toString());
+        LocalTime localTime = null;
+        while (m.find()) {
+            int hour = Integer.parseInt(m.group(1));
+            int min = Integer.parseInt(m.group(2));
+            int second = 0;
+            if (m.groupCount() >= 4) {
+                second = Integer.parseInt(m.group(3));
+            }
+            int nanos = 0;
+            if (m.groupCount() >= 5) {
+                nanos = Integer.parseInt(m.group(4));
+            }
+            localTime = LocalTime.of(hour, min, second, nanos);
+        }
+        return localTime;
+    }),
+    DATE_TIME("datetime", "string", "yyyy-MM-dd HH:mm:ss", LocalDateTime.class, "DATETIME", Pattern.compile("^([1-9]\\d{3})[-/.](0[1-9]|1[0-2])[-/.](0[1-9]|[1-2][0-9]|3[0-1])[ |T](20|21|22|23|[0-1]\\d):([0-5]\\d)(?:\\:([0-5]\\d)){0,1}(?:\\.(\\d{3})){0,1}"), (p, v) -> {
+        Matcher m = p.matcher(v.toString());
+        LocalDateTime localDateTime = null;
+        while (m.find()) {
+            int year = Integer.parseInt(m.group(1));
+            int month = Integer.parseInt(m.group(2));
+            int day = Integer.parseInt(m.group(3));
+            int hour = Integer.parseInt(m.group(4));
+            int min = Integer.parseInt(m.group(5));
+            int second = 0;
+            int nanos = 0;
+            if (m.groupCount() >= 7) {
+                second = Integer.parseInt(m.group(6));
+            }
+            if (m.groupCount() >= 8) {
+                nanos = Integer.parseInt(m.group(7));
+            }
+            localDateTime = LocalDateTime.of(year, month, day, hour, min, second, nanos);
+        }
+        return localDateTime;
+    }),
+    DOUBLE("double", "number", "00.0",
+            Double.class, "DOUBLE", Pattern.compile("^[-]{0,1}(\\d+)[.]{0,1}(\\d+)[dD]{0,1}"), (p, v) -> {
+        if (v instanceof Number) {
+            return ((Number) v).doubleValue();
+        }
+        return Double.valueOf(v.toString());
+    }),
+    CURRENCY("currency", "number", "bigDecimal format",
+            BigDecimal.class, "DECIMAL", Pattern.compile("^[-]{0,1}([￥$€￡￠])(\\d+)[.]{0,1}(\\d+)"), (p, v) -> {
+        BigDecimal decimal = null;
+        String vv = v.toString();
+        Matcher m = p.matcher(vv);
+        long unscaledVal = 1;
+        if (vv.startsWith("-")) {
+            unscaledVal = -1;
+        }
+        while (m.find()) {
+            unscaledVal = Long.parseLong(m.group(2)) * unscaledVal;
+            int ps = 0;
+            if (m.groupCount() >= 4) {
+                ps = Integer.parseInt(m.group(3));
+            }
+            decimal = BigDecimal.valueOf(unscaledVal, ps);
+        }
+        return decimal;
+    }),
+    TIMESTAMP("timestamp", "number", "millisecond",
+            Long.class, "TIMESTAMP", null, (p, v) -> {
+        return Long.valueOf(v.toString());
+    }),
+    YEAR_MONTH("ym", "number", "yyyyMM",
+            Integer.class, "INT", Pattern.compile("^([1-9]\\d{3})(0[1-9]|1[0-2])"), (p, v) -> {
+        LocalDate date = null;
+        Matcher m = p.matcher(v.toString());
+        if (m.find()) {
+            int year = Integer.parseInt(m.group(1));
+            int mm = Integer.parseInt(m.group(2));
+            date = LocalDate.of(year, mm, 1);
+        }
+        return date;
+    }),
+    YEAR_MONTH_DATE("ymd", "number", "yyyyMMdd",
+            Integer.class, "INT", Pattern.compile("^([1-9]\\d{3})(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])"), (p, v) -> {
+        LocalDate date = null;
+        Matcher m = p.matcher(v.toString());
+        if (m.find()) {
+            int year = Integer.parseInt(m.group(1));
+            int mm = Integer.parseInt(m.group(2));
+            int day = Integer.parseInt(m.group(3));
+            date = LocalDate.of(year, mm, day);
+        }
+        return date;
+    }),
+    MONTH("mm", "number", "MM",
+            Integer.class, "INT", null, (p, v) -> {
+        if (v instanceof Number) {
+            return ((Number) v).intValue();
+        }
+        return Integer.parseInt(v.toString());
+    }),
+    MONTH_DATE("md", "number", "MMdd",
+            Integer.class, "INT", null, (p, v) -> {
+        if (v instanceof Number) {
+            return ((Number) v).intValue();
+        }
+        return Integer.parseInt(v.toString());
+    }),
+    DAY_OF_MONTH("dm", "number", "dd",
+            Integer.class, "INT", null, (p, v) -> {
+        if (v instanceof Number) {
+            return ((Number) v).intValue();
+        }
+        return Integer.parseInt(v.toString());
+    }),
+    MAP("map", "json", "",
+            Map.class, "TEXT", null, (p, v) -> v),
+    LIST("list", "list", "",
+            List.class, "TEXT", null, (p, v) -> v),
+    COLLECTION("seq", "list", "list,set, array",
+            Collection.class, "TEXT", null, (p, v) -> v),
+    NUMBER("number", "number", "int,double float,long",
+            Number.class, "DOUBLE", null, (p, v) -> v),
+    DECIMAL("decimal", "number", "decimal type",
+            BigDecimal.class, "DECIMAL", null, (p, v) -> {
+        try {
+            DecimalFormat df = new DecimalFormat();
+            Number vv = df.parse(v.toString());
+            return new BigDecimal(vv.toString());
+        } catch (ParseException e) {
+            return BigDecimal.ZERO;
+        }
+    }),
+    SET("set", "list", "",
+            Set.class, "TEXT", null, (p, v) -> v),
+    OBJECT("object", "json", "",
+            Object.class, "Blob", null, (p, v) -> v),
+    FILE("object", "json", "",
+            File.class, "Blob", null, (p, v) -> v),
+
+    NUMERIC("numeric", "number", "00.0", Double.class, "DOUBLE", null, (p, v) -> v),
+    FRAGMENT("string", "string", "character string", String.class, "VARCHAR", null, (p, v) -> v);
 
 
-    private String description;
+    private final String description;
 
-    private String grouping;
+    private final String grouping;
 
-    private String brief;
+    private final String brief;
 
-    private Class<?> clazz;
+    private final Class<?> clazz;
 
-    private String dbFieldType;
+    private final String dbFieldType;
 
-    private String[] patterns;
+    private final Pattern pattern;
+
+    private final BiFunction<Pattern, Object, Object> converter;
 
 
-    FieldValueType(String brief, String grouping, String description, Class<?> clazz, String dbFieldType,String[] patterns) {
+    FieldValueType(String brief, String grouping, String description,
+                   Class<?> clazz, String dbFieldType, Pattern pattern, BiFunction<Pattern, Object, Object> converter) {
         this.brief = brief;
         this.grouping = grouping;
         this.description = description;
         this.clazz = clazz;
         this.dbFieldType = dbFieldType;
-        this.patterns = patterns;
+        this.pattern = pattern;
+        this.converter = converter;
     }
 
     public static FieldValueType getTypeByBrief(String brief) {
@@ -135,7 +303,6 @@ public enum FieldValueType {
         }
     }
 
-
     public static FieldValueType convertJavaTypeByValue(Object value) {
         if (value instanceof Integer) {
             return FieldValueType.INTEGER;
@@ -158,8 +325,8 @@ public enum FieldValueType {
         } else if (value instanceof String) {
             return FieldValueType.STRING;
         } else if (value instanceof Map) {
-            Map<String,Object> m = (Map<String, Object>) value;
-            if(m.containsKey("resourceId") && m.containsKey("uri")){
+            Map<String, Object> m = (Map<String, Object>) value;
+            if (m.containsKey("resourceId") && m.containsKey("uri")) {
                 return FieldValueType.FILE;
             }
             return FieldValueType.MAP;
@@ -172,25 +339,43 @@ public enum FieldValueType {
         }
     }
 
+    public Object normalize(Object value) {
+        return converter.apply(pattern, value);
+    }
+
+    public Object normalize(Object value, String format) {
+        Object val = normalize(value);
+        if (val instanceof TemporalAccessor) {
+            return DateTimeFormatter.ofPattern(format).format((TemporalAccessor) val);
+        }
+
+        val = String.format(format,val);
+        if(this.isNumber()){
+            return normalize(val);
+        }
+
+        return val;
+    }
+
+
     private static boolean isDate(Object value) {
-        return isDateString("^[1-9]\\d{3}([-/.])(0[1-9]|1[0-2])([-/.])(0[1-9]|[1-2][0-9]|3[0-1])", value);
+        return patternMatch(DATE.pattern, value);
     }
 
     private static boolean isTime(Object value) {
-        return isDateString("^(20|21|22|23|[0-1]\\d):[0-5]\\d(:[0-5]\\d){0,1}", value);
+        return patternMatch(TIME.pattern, value);
     }
 
     private static boolean isDateTime(Object value) {
-        return isDateString("^([1-9]\\d{3})[-/.](0[1-9]|1[0-2])[-/.](0[1-9]|[1-2][0-9]|3[0-1])[ |T](20|21|22|23|[0-1]\\d):([0-5]\\d)(?:\\:([0-5]\\d)){0,1}(?:\\.(\\d{3})){0,1}", value);
+        return patternMatch(DATE_TIME.pattern, value);
     }
 
-    private static boolean isDateString(String format, Object value) {
+    private static boolean patternMatch(Pattern pattern, Object value) {
         try {
-            return String.valueOf(value).matches(format);
+            return pattern.matcher(String.valueOf(value)).matches();
         } catch (Exception e) {
             return false;
         }
     }
-
 
 }
