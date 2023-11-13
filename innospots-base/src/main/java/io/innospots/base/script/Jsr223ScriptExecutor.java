8a2a6219957bb64dde19f6a9ecb117c5f88e53d3
Smars <smars@innospots.io>
2023-11-13 17:39:02 +0800

    feat: create 2.0

---
+++ innospots-base/src/main/java/io/innospots/base/script/Jsr223ScriptExecutor.java
@@ -0,0 +1,17 @@
+package io.innospots.base.script;
+
+import io.innospots.base.exception.ScriptException;
+
+import java.util.Map;
+
+/**
+ * @author Smars
+ * @date 2023/11/13
+ */
+public class Jsr223ScriptExecutor implements IScriptExecutor {
+
+    @Override
+    public Object execute(Map<String, Object> env) throws ScriptException {
+        return null;
+    }
+}
